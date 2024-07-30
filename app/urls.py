bbe1ab8f79773b3f52ce43b38d547a4918cd3385
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-07-30 15:40:26 -0300

    Criação da entidade Genero

--- app/urls.py
+++ app/urls.py
@@ -9,12 +9,13 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import CategoriaViewSet, UserViewSet
+from core.views import CategoriaViewSet, GeneroViewSet, UserViewSet
 
 router = DefaultRouter()
 
 router.register(r"usuarios", UserViewSet, basename="usuarios")
 router.register(r"categorias", CategoriaViewSet, basename="categorias")
+router.register(r"generos", GeneroViewSet, basename="generos")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,2 +1,3 @@
 from .user import UserViewSet
 from .categoria import CategoriaViewSet
+from .genero import GeneroViewSet
