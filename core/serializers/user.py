c096508d5fb190531190a6e0b9e094455c02114e
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-09-11 16:00:34 -0300

    Criador do Serializador Trabalhador Pequeno

--- core/serializers/user.py
+++ core/serializers/user.py
@@ -7,3 +7,9 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+
+class TrabalhadorPequeno(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("foto", "nome", "nota", "categoria") 
